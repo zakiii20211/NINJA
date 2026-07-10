f02eb830f1629a25aadbe6af608ae47deeb55071
zakiii20211 <zakiii20211@gmail.com>
2026-07-10 22:10:14 +0800

    Update ssh-vpn.sh

--- sshws/ssh-vpn.sh
+++ sshws/ssh-vpn.sh
@@ -313,7 +313,9 @@ wget -O delete "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/de
 wget -O add-host "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/add-host.sh"
 wget -O speedtest "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/speedtest_cli.py"
 wget -O restart-service "https://cdn.jsdelivr.net/gh/zakiii20211/NINJA@main/menu/restart-service.sh"
-wget -O ram "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/ram.sh"
+wget -O ram "apt update -y && apt install -y python3
+rm ram.sh*
+wget https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/ram.sh"
 wget -O info "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/info.sh"
 wget -O nf "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/nf.sh"
 wget -O mdns "https://raw.githubusercontent.com/zakiii20211/NINJA/main/menu/mdns.sh"
