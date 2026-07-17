3f711dc74f36e2fe3fde30a1a01cb54929c5a8cd
zakiii20211 <zakiii20211@gmail.com>
2026-07-17 13:20:30 +0800

    Update kemaskini.sh

--- menu/kemaskini.sh
+++ menu/kemaskini.sh
@@ -8,7 +8,7 @@ rm -f /usr/local/bin/add-xvless
 rm -f /usr/local/bin/trial-xvless
 # download script
 cd /usr/local/bin
-wget -O add-xvless "${websc}/script/lifetime/upgrade/add-xvless.sh" && chmod +x add-xvless
+wget -O mxray.sh "https://raw.githubusercontent.com/zakiii20211/NINJA/main/xray/mxray.sh" && chmod +x mxray.sh
 wget -O trial-xvless "${websc}/script/lifetime/upgrade/trial-xvless.sh" && chmod +x trial-xvless
 cd
 clear
